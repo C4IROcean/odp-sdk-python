ec895c4ee674acd2161fcbdb9707c53e52e76ba1
ocean-kristian <67058812+ocean-kristian@users.noreply.github.com>
2020-07-02 12:59:48 +0200

    Update __init__.py

--- odp_sdk/__init__.py
+++ odp_sdk/__init__.py
@@ -1,6 +1,2 @@
-#from cognite.power.client import PowerClient
-#from cognite.power.data_classes import PowerAsset, PowerAssetList
-#from cognite.power.power_area import PowerArea
-#from cognite.power.power_corridor import PowerCorridor, PowerCorridorComponent
-#from cognite.power.power_graph import PowerGraph
-from .odp_sdk import *
+
+from ODP-SDK.odp_sdk import client
