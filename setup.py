dbc5ce4493d19ad11e7ff63f6ae743b3d2142ec1
ocean-kristian <67058812+ocean-kristian@users.noreply.github.com>
2020-07-03 09:21:27 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import setuptools
+
+
+setuptools.setup(
+    name="odp_sdk",
+    version="0.0.14",  # Update this for every new version
+    #py_modules = ['client'],
+    author="ocean-kristian",
+    author_email="kristian.authen@oceandata.earth",
+    description='Python SDK for the Ocean Data Platform',
+    long_description="Connect to the Ocean Data Platform with Python through the Python SDK. Download queried ocean data easily and efficiently into data frames, for easy exploring and further processing in your data science project.",
+    long_description_content_type="text/markdown",
+    install_requires=["cognite-sdk>=1.8",                      # Add project dependencies here
+        "pandas>=0.20.0"                    # example: pandas version 0.20 or greater                          
+    ],                                             
+    url='https://github.com/C4IROcean/odp',  
+    packages=setuptools.find_packages(),
+    classifiers=(                                 # Classifiers help people find your 
+        "Programming Language :: Python :: 3",    # projects. See all possible classifiers 
+        "License :: OSI Approved :: Apache Software License", # in https://pypi.org/classifiers/
+        "Operating System :: OS Independent",   
+    ),
+)
+
+
+#python setup.py sdist bdist_wheel 
+#twine upload --repository testpypi dist/*
+
