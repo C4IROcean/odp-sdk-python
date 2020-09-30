f4256f6166cd0d5e77c82fdfea83193149852c7d
Thomas Li Fredriksen <thomas.fredriksen@evry.com>
2020-09-30 15:50:08 +0200

    Updated sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,11 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-import re
+from os import path
+import sys
+sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), "../../")))
 
 
 # -- Project information -----------------------------------------------------
@@ -66,3 +64,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = "index"
