b7133143e5587d9b978a144d9a53f2742f4c2f8a
Thomas Fredriksen <thomasfredriksen@Thomass-Laptop.localdomain>
2023-11-25 22:23:14 +0100

    Added NDJSON parser

---
+++ odp_sdk/utils/ndjson.py
@@ -0,0 +1,98 @@
+from collections import deque
+from io import StringIO, BytesIO
+import json
+from typing import Optional, IO, Deque, Iterable, cast
+from warnings import warn
+
+from .json import JsonType, JsonParser
+
+
+class NdJsonParser:
+    def __init__(
+        self,
+        s: Optional[str | bytes] = None,
+        fp: Optional[IO] = None,
+        json_parser: JsonParser = json,
+    ):
+        self.json_parser = json_parser
+        self.line = ""
+        self.delimiter_stack: Deque[str] = deque()
+
+        if s and fp:
+            raise ValueError("Either 's' or 'fp' must be set, but now both")
+        elif not s and not fp:
+            raise ValueError("Either 's' or 'fp' must be set")
+
+        self.fb = fp if fp else (StringIO(s) if isinstance(s, str) else BytesIO(s))
+
+    def consume_line(self) -> JsonType:
+        if self.delimiter_stack:
+            warn(
+                "Attempting to parse NDJSON line while the delimiter stack was non-empty"
+            )
+
+        obj = self.json_parser.loads(self.line)
+        self.line = ""
+        self.delimiter_stack.clear()
+
+        return obj
+
+    def __iter__(self) -> Iterable[JsonType]:
+        return cast(Iterable[JsonType], self)
+
+    def __next__(self) -> JsonType:
+        while True:
+            try:
+                s = next(self.fb)
+            except StopIteration:
+                if self.line:
+                    return self.consume_line()
+                raise
+
+            for c in s:
+                if isinstance(c, int):
+                    c = chr(c)
+                last_delimiter = (
+                    self.delimiter_stack[-1] if self.delimiter_stack else None
+                )
+
+                in_quote = last_delimiter in {"'", '"', "\\"}
+
+                if c == "\n" and not in_quote:
+                    return self.consume_line()
+
+                self.line += c
+                if in_quote:
+                    if last_delimiter == "\\":
+                        self.delimiter_stack.pop()
+                    elif c == "\\":
+                        self.delimiter_stack.append(c)
+                    elif c == last_delimiter:
+                        self.delimiter_stack.pop()
+
+                    continue
+
+                is_quote = c in {"'", '"'}
+                if is_quote:
+                    self.delimiter_stack.append(c)
+                    continue
+
+                is_opening_bracket = c in {"{", "["}
+
+                if is_opening_bracket:
+                    self.delimiter_stack.append(c)
+                    continue
+
+                in_bracket = last_delimiter in {"{", "["}
+                is_closing_bracket = c in {"}", "]"}
+
+                if is_closing_bracket:
+                    if not in_bracket:
+                        raise ValueError(f"Got unexpected delimiter: {c}")
+
+                    if last_delimiter == "{" and c == "}":
+                        self.delimiter_stack.pop()
+                    elif last_delimiter == "[" and c == "]":
+                        self.delimiter_stack.pop()
+                    else:
+                        raise ValueError(f"Got unexpected delimiter: {c}")
---
+++ test/test_unit/test_utils/test_ndjson.py
@@ -0,0 +1,109 @@
+from textwrap import dedent
+from odp_sdk.utils.ndjson import NdJsonParser
+
+
+def test_parse_ndjson_simple():
+    test_str = dedent(
+        """
+        {"name": "Alice", "age": 30}
+        {"name": "Bob", "age": 25}
+        {"name": "Charlie", "age": 35}
+        """
+    ).strip()
+
+    ndjson_parser = NdJsonParser(s=test_str)
+
+    parsed_rows = list(iter(ndjson_parser))
+
+    assert isinstance(parsed_rows, list)
+    assert len(parsed_rows) == 3
+
+    assert parsed_rows[0]["name"] == "Alice"
+    assert parsed_rows[0]["age"] == 30
+    assert parsed_rows[1]["name"] == "Bob"
+    assert parsed_rows[1]["age"] == 25
+    assert parsed_rows[2]["name"] == "Charlie"
+    assert parsed_rows[2]["age"] == 35
+
+
+def test_parse_ndjson_binary_simple():
+    test_str = (
+        dedent(
+            """
+            {"product_id": 1, "name": "Widget", "price": 10.99}
+            {"product_id": 2, "name": "Gadget", "price": 19.99}
+            {"product_id": 3, "name": "Tool", "price": 15.49}
+            """
+        )
+        .strip()
+        .encode("utf-8")
+    )
+
+    ndjson_parser = NdJsonParser(s=test_str)
+
+    parsed_rows = list(iter(ndjson_parser))
+
+    assert isinstance(parsed_rows, list)
+    assert len(parsed_rows) == 3
+
+    assert parsed_rows[0]["product_id"] == 1
+    assert parsed_rows[0]["name"] == "Widget"
+    assert parsed_rows[0]["price"] == 10.99
+    assert parsed_rows[1]["product_id"] == 2
+    assert parsed_rows[1]["name"] == "Gadget"
+    assert parsed_rows[1]["price"] == 19.99
+    assert parsed_rows[2]["product_id"] == 3
+    assert parsed_rows[2]["name"] == "Tool"
+    assert parsed_rows[2]["price"] == 15.49
+
+
+def test_parse_ndjson_special_characters():
+    test_str = dedent(
+        """
+        {"fruits": ["apple", "banana", "cherry"], "description": "Delicious & healthy 🍏🍌🍒"}
+        {"colors": ["red", "green", "blue"], "symbols": ["@#$%^&*()_+!"]}
+        {"languages": ["English", "Español", "Français"], "special_chars": "ñçüëł"}
+        """
+    ).strip()
+
+    ndjson_parser = NdJsonParser(s=test_str)
+    parsed_rows = list(iter(ndjson_parser))
+
+    assert isinstance(parsed_rows, list)
+    assert len(parsed_rows) == 3
+
+    assert parsed_rows[0]["fruits"] == ["apple", "banana", "cherry"]
+    assert parsed_rows[0]["description"] == "Delicious & healthy 🍏🍌🍒"
+    assert parsed_rows[1]["colors"] == ["red", "green", "blue"]
+    assert parsed_rows[1]["symbols"] == ["@#$%^&*()_+!"]
+    assert parsed_rows[2]["languages"] == ["English", "Español", "Français"]
+    assert parsed_rows[2]["special_chars"] == "ñçüëł"
+
+
+def test_parse_ndjson_embedded_json():
+    test_str = dedent(
+        """
+        {"content": "Nested objects: {\\\"key1\\\": \\\"value1\\\", \\\"key2\\\": \\\"value2\\\"}"}
+        {"config": "{ \\\"param1\\\": [1, 2, 3], \\\"param2\\\": {\\\"a\\\": true, \\\"b\\\": false} }"}
+        {"formula": "Mathematical expressions: {\\\"equation\\\": \\\"x^2 + y^2 = r^2\\\"}"}
+        """
+    ).strip()
+
+    ndjson_parser = NdJsonParser(s=test_str)
+    parsed_rows = list(iter(ndjson_parser))
+
+    assert isinstance(parsed_rows, list)
+    assert len(parsed_rows) == 3
+
+    assert (
+        parsed_rows[0]["content"]
+        == 'Nested objects: {"key1": "value1", "key2": "value2"}'
+    )
+    assert (
+        parsed_rows[1]["config"]
+        == '{ "param1": [1, 2, 3], "param2": {"a": true, "b": false} }'
+    )
+    assert (
+        parsed_rows[2]["formula"]
+        == 'Mathematical expressions: {"equation": "x^2 + y^2 = r^2"}'
+    )
