bd867d60b453b11599a21187197959ec2a56f964
Thomas Li Fredriksen <thomas.fredriksen@oceandata.earth>
2023-11-26 12:07:51 +0100

    Added some ndjson docstrings

--- odp_sdk/utils/ndjson.py
+++ odp_sdk/utils/ndjson.py
@@ -6,14 +6,28 @@ from warnings import warn
 
 from .json import JsonParser, JsonType
 
+DEFAULT_JSON_PARSER = cast(JsonParser, json)
+
 
 class NdJsonParser:
+    """Newline delimited JSON parser
+
+    Parses NDJSON from a file-like object or a string.
+    """
+
     def __init__(
         self,
         s: Optional[str | bytes] = None,
         fp: Optional[IO] = None,
-        json_parser: JsonParser = json,
+        json_parser: JsonParser = DEFAULT_JSON_PARSER,
     ):
+        """Initialize the parser
+
+        Args:
+            s: String to parse, either this or 'fp' must be set
+            fp: File-like object to parse, either this or 's' must be set
+            json_parser: JSON parser to use, defaults to the standard `json` module
+        """
         self.json_parser = json_parser
         self.line = ""
         self.delimiter_stack: Deque[str] = deque()
@@ -25,7 +39,12 @@ class NdJsonParser:
 
         self.fb = fp if fp else (StringIO(s) if isinstance(s, str) else BytesIO(s))
 
-    def consume_line(self) -> JsonType:
+    def _consume_line(self) -> JsonType:
+        """Consume a line from the file-like object
+
+        Returns:
+            Parsed JSON object
+        """
         if self.delimiter_stack:
             warn("Attempting to parse NDJSON line while the delimiter stack was non-empty")
 
@@ -44,7 +63,7 @@ class NdJsonParser:
                 s = next(self.fb)
             except StopIteration:
                 if self.line:
-                    return self.consume_line()
+                    return self._consume_line()
                 raise
 
             for c in s:
@@ -55,7 +74,7 @@ class NdJsonParser:
                 in_quote = last_delimiter in {"'", '"', "\\"}
 
                 if c == "\n" and not in_quote:
-                    return self.consume_line()
+                    return self._consume_line()
 
                 self.line += c
                 if in_quote:
@@ -92,3 +111,29 @@ class NdJsonParser:
                         self.delimiter_stack.pop()
                     else:
                         raise ValueError(f"Got unexpected delimiter: {c}")
+
+
+def load(fp: IO, json_parser: JsonParser = DEFAULT_JSON_PARSER) -> Iterable[JsonType]:
+    """Load NDJSON from a file-like object
+
+    Args:
+        fp: File-like object
+        json_parser: JSON parser to use
+
+    Returns:
+        Iterable of parsed JSON objects
+    """
+    return iter(NdJsonParser(fp=fp, json_parser=json_parser))
+
+
+def loads(s: str | bytes, json_parser: JsonParser = DEFAULT_JSON_PARSER) -> Iterable[JsonType]:
+    """Load NDJSON from a string
+
+    Args:
+        s: String
+        json_parser: JSON parser to use
+
+    Returns:
+        Iterable of parsed JSON objects
+    """
+    return iter(NdJsonParser(s=s, json_parser=json_parser))
