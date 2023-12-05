a39503486989dc9f0ef2b6dca671d93ea0cd076b
Henrik Torget <henrik.torget@oceandata.earth>
2023-12-05 14:18:44 +0100

    Adds list files

--- tests/test_raw_storage_client.py
+++ tests/test_raw_storage_client.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 import responses
 from uuid import uuid4
@@ -88,3 +90,51 @@ def test_get_file_metadata_not_found(raw_storage_client):
 
         with pytest.raises(OdpFileNotFoundError):
             raw_storage_client.get_file_metadata(dataset_dto, file_ref)
+
+
+def test_list_files_success(raw_storage_client):
+    dataset_uuid = uuid4()
+    dataset_name = "test_dataset"
+    dataset_kind = "catalog.hubocean.io/dataset"
+    dataset_version = "v1alpha3"
+    dataset_spec = {
+        "storage_class": "registry.hubocean.io/storageClass/raw",
+        "maintainer": {
+            "contact": "HUB Ocean",
+            "organisation": "HUB Ocean"
+        },
+        "documentation": [
+            "https://oceandata.earth"
+        ],
+        "tags": ["test", "hubocean"]
+
+    }
+
+    metadata = MetadataDto(name=dataset_name, uuid=dataset_uuid)
+
+    dataset_dto = DatasetDto(spec=dataset_spec,
+                                kind=dataset_kind,
+                                version=dataset_version,
+                                metadata=metadata)
+
+    file_metadata_name = "file.zip"
+    file_metadata_mime_type = "application/zip"
+    file_metadata = FileMetadata(
+        name=file_metadata_name,
+        mime_type=file_metadata_mime_type,
+    )
+
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            responses.POST,
+            f"{raw_storage_client.raw_storage_url}/{dataset_uuid}/list",
+            json=[json.loads(file_metadata.model_dump_json())],
+            status=200,
+            content_type="application/json",
+        )
+
+        result = raw_storage_client.list_files(dataset_dto)
+
+        assert result[0].name == file_metadata_name
+        assert result[0].mime_type == file_metadata_mime_type
+
