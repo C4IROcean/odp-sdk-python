cb03f399adbbab9bdebff44cb7495b1a669c2889
Henrik Torget <henrik.torget@hubocean.earth>
2023-12-22 11:00:58 +0100

    Fix tests

--- tests/test_unit/test_raw_storage_client.py
+++ tests/test_unit/test_raw_storage_client.py
@@ -14,77 +14,82 @@ def raw_storage_client(http_client) -> OdpRawStorageClient:
 
 
 def test_get_file_metadata_success(raw_storage_client, raw_resource_dto):
-    file_ref = "/home/hubocean/file.zip"
-
-    file_meta = FileMetadataDto(file_metadata_name="file.zip", file_metadata_mime_type="application/zip")
+    file_meta = FileMetadataDto(
+        file_metadata_name="file.zip", file_metadata_mime_type="application/zip"
+    )
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.GET,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_ref}/metadata",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_meta.name}/metadata",
             body=file_meta.model_dump_json(),
             status=200,
             content_type="application/json",
         )
 
-        result = raw_storage_client.get_file_metadata(raw_resource_dto, file_ref)
+        result = raw_storage_client.get_file_metadata(raw_resource_dto, file_meta)
 
         assert result.name == file_meta.name
         assert result.mime_type == file_meta.mime_type
 
 
 def test_get_file_metadata_not_found(raw_storage_client, raw_resource_dto):
-    file_ref = "non_existent_file_ref"
+    file_meta = FileMetadataDto(
+        file_metadata_name="file.zip", file_metadata_mime_type="application/zip"
+    )
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.GET,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_ref}/metadata",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_meta.name}/metadata",
             status=404,
         )
 
         with pytest.raises(OdpFileNotFoundError):
-            raw_storage_client.get_file_metadata(raw_resource_dto, file_ref)
+            raw_storage_client.get_file_metadata(raw_resource_dto, file_meta)
 
 
 def test_list_files_success(raw_storage_client, raw_resource_dto):
-    file_metadata_name = "file.zip"
-    file_metadata_mime_type = "application/zip"
     file_metadata = FileMetadataDto(
-        name=file_metadata_name,
-        mime_type=file_metadata_mime_type,
+        name="file.zip",
+        mime_type="application/zip",
     )
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.POST,
             f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/list",
-            json={"results": [json.loads(file_metadata.model_dump_json())], "next": None, "num_results": 1},
+            json={
+                "results": [json.loads(file_metadata.model_dump_json())],
+                "next": None,
+                "num_results": 1,
+            },
             status=200,
             content_type="application/json",
         )
 
-        metadata_filter = {"name": file_metadata_name}
+        metadata_filter = {"name": file_metadata.name}
 
-        result = raw_storage_client.list(raw_resource_dto, metadata_filter=metadata_filter)
+        result = raw_storage_client.list(
+            raw_resource_dto, metadata_filter=metadata_filter
+        )
 
         first_item = next(iter(result))
 
-        assert first_item.name == file_metadata_name
-        assert first_item.mime_type == file_metadata_mime_type
+        assert first_item.name == file_metadata.name
+        assert first_item.mime_type == file_metadata.mime_type
 
 
 def test_create_file_success(raw_storage_client, raw_resource_dto):
-    file_name = "new_file.txt"
     file_metadata = FileMetadataDto(
-        name=file_name,
+        name="new_file.txt",
         mime_type="text/plain",
     )
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.POST,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_metadata.name}",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}",
             status=200,
             json=json.loads(file_metadata.model_dump_json()),
             content_type="application/json",
@@ -92,15 +97,17 @@ def test_create_file_success(raw_storage_client, raw_resource_dto):
 
         rsps.add(
             responses.GET,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_name}/metadata",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_metadata.name}/metadata",
             json=json.loads(file_metadata.model_dump_json()),
             status=200,
             content_type="application/json",
         )
 
-        result = raw_storage_client.create_file(raw_resource_dto, file_meta=file_metadata, contents=None)
+        result = raw_storage_client.create_file(
+            raw_resource_dto, file_metadata_dto=file_metadata, contents=None
+        )
 
-        assert result.name == file_name
+        assert result.name == file_metadata.name
         assert result.mime_type == "text/plain"
 
 
@@ -108,17 +115,19 @@ def test_download_file_save(raw_storage_client, raw_resource_dto, tmp_path):
     file_data = b"Sample file content"
     save_path = tmp_path / "downloaded_file.txt"
 
-    file_meta_data = FileMetadataDto(name="test_file.txt", mime_type="text/plain")
+    file_metadata = FileMetadataDto(name="test_file.txt", mime_type="text/plain")
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.GET,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_meta_data.name}",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_metadata.name}",
             body=file_data,
             status=200,
         )
 
-        raw_storage_client.download_file(raw_resource_dto, file_meta_data, save_path=str(save_path))
+        raw_storage_client.download_file(
+            raw_resource_dto, file_metadata, save_path=str(save_path)
+        )
 
         with open(save_path, "rb") as file:
             saved_data = file.read()
@@ -127,14 +136,14 @@ def test_download_file_save(raw_storage_client, raw_resource_dto, tmp_path):
 
 
 def test_delete_file_not_found(raw_storage_client, raw_resource_dto):
-    filename = "test_file.txt"
+    file_metadata = FileMetadataDto(name="test_file.txt", mime_type="text/plain")
 
     with responses.RequestsMock() as rsps:
         rsps.add(
             responses.DELETE,
-            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{filename}",
+            f"{raw_storage_client.raw_storage_url}/{raw_resource_dto.metadata.uuid}/{file_metadata.name}",
             status=404,  # Assuming status code 404 indicates file not found
         )
 
         with pytest.raises(OdpFileNotFoundError):
-            raw_storage_client.delete_file(raw_resource_dto, filename)
+            raw_storage_client.delete_file(raw_resource_dto, file_metadata)
