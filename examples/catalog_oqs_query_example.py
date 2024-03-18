8b1a466a91c98518a5c1f92ea7601230eb4e7395
Henrik Torget <henrik.torget@oceandata.earth>
2024-03-18 11:42:26 +0100

    pre commit

--- examples/catalog_oqs_query_example.py
+++ examples/catalog_oqs_query_example.py
@@ -5,12 +5,12 @@ client = OdpClient()
 # Filter collections
 
 oqs_filter = {
-  "oqs": { # When querying to the catalog, the oqs filter is passed inside an object named oqs.
-    "#EQUALS": [ #EQUALS is used here to compare to values
-      "$kind", #The first value is the kind from the metadata, prefaced with a dollarsign.
-      "catalog.hubocean.io/dataCollection" # And this is the value to compare with
-    ]
-  }
+    "oqs": {  # When querying to the catalog, the oqs filter is passed inside an object named oqs.
+        "#EQUALS": [  # EQUALS is used here to compare to values
+            "$kind",  # The first value is the kind from the metadata, prefaced with a dollarsign.
+            "catalog.hubocean.io/dataCollection",  # And this is the value to compare with
+        ]
+    }
 }
 
 
